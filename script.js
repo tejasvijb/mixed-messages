e25226a404bc9abc0e829c7baec96d48114843dd
tejasvijb <jbtejasvi@gmail.com>
2022-09-30 20:28:33 +0530

    create-datasets

---
+++ script.js
@@ -0,0 +1,100 @@
+// Trying to replicate a populer card game called cards against humanity
+
+// Create datasets
+
+const cards = {
+  card1: `Lord, please guide my baby girl back to you and away from __________  Amen.`,
+  card2: `What never fails to liven up the party?`,
+  card3: `You're on a first date. What's an instant red flag?`,
+  card4: `Rub a dub dub, ______________in a tub!`,
+  card5: `Ready or not, here comes ______________!`,
+  card6: `If you can't be with the one you love, love ____________`,
+  card7: `A new adventure awaits at Walt Disney's Magical Kingdom of ____________`,
+  card8: `Me: Hey, Randy! What's going on? Randy: Aw nothing, man. Just ____________`,
+  card9: `My vagina is ____________`,
+  card10: `Holy MOLY! Now THAT'S what I call ______________`,
+};
+
+const data = {
+  card1: [
+    'Saying Everything is okay when you everything is clearly not okay',
+    'The Boy Scouts of America.',
+    'Getting into a pretty bad car accident.',
+    'Geese',
+    'Hot cheese',
+    'Three dicks at the same time'
+  ],
+  card2: [ 
+    'Foreskin.',
+    'Pixelated bukkake.',
+    'The harsh reality that all horses are people in horse costumes.',
+    'Some cracker-ass motherfucker.',
+    'Climbing a telephone pole and becoming one with the T-Mobile network.'
+  ],
+  card3: [
+    '..it is unclear.',
+    'Having a stroke.',
+    'Hot people.',
+    'The beautiful backpacks of JanSport.',
+    'Leprosy.',
+    'Small plates, or tapas if you will.'
+  ],
+  card4: [
+    `What the State of New Jersey describes as "public defecation."`,
+    'Hard-hitting investigative journalism.',
+    'Tentacles.',
+    'Watching you poop.',
+    'Just a Luna bar for lunch.',
+    'Fading away into nothingness.'
+  ],
+  card5: [
+    'The screams... the terrible screams.',
+    'Domestic terrorism.',
+    'Itchy pussy.',
+    'Tongue-fucking them toes.',
+    'Chinese President Xi Jinping.',
+    'Faking a pregnancy'
+  ],
+  card6: [
+    'The Annual San Diego Conference of Blowjobs.',
+    'Telling a shitty story that goes nowhere.',
+    'Three men and three hot dogs.',
+    'Rectangles.',
+    'Hentai.',
+    'Violating the Geneva Convention.'
+  ],
+  card7: [
+    'God.',
+    'The clitoris.',
+    'Lying face-down in wet cement.',
+    'The Electoral College.',
+    'Expecting a burp and vomiting on the floor.',
+    'A really cool hat.'
+  ],
+  card8: [
+    'My cheating son-of-a-bitch husband.',
+    'Racially-biased SAT questions.',
+    'Having six weeks left to live.',
+    'Hush money.',
+    'A sexually charged game of chess.',
+    'Shitting into a coinstart machine.'
+  ],
+  card9: [
+    'Penis envy.',
+    'What I assume to be semen.',
+    'A narc.',
+    'Girls.',
+    'Bananas.',
+    'Clean drinking water.'
+  ],
+  card10: [
+    'Getting white-girl wasted.',
+    'A narc.',
+    `Daniel Radcliffe's delicious asshole.`,
+    'German dungeon porn.',
+    'Blue smoke coming out of my anus',
+    'Prettending to be a dentist'
+  ]
+};
+
+
